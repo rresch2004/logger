76adf5abc682aaf6dd9afe83614a46120c5ae614
rresch2004 <59953979+rresch2004@users.noreply.github.com>
2023-03-21 10:16:07 +0100

    add FileLogger

--- src/EchoLogger.php
+++ src/EchoLogger.php
@@ -1,10 +1,10 @@
 <?php
 namespace Rresch2004\Logger;
 
-class EchoLogger implements ILogger{
+class EchoLogger implements ILogger {
 
     public function logEntry(string $line): void
     {
-        echo '[LOG]'$line . "/n";
+        echo '[LOG]: ' . $line . "\n";
     }
 }
---
+++ src/FileLogger.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Mbacher1810\Logger;
+
+class FileLogger implements ILogger
+{
+    private $handle = null;
+
+    public function __construct(string $filename)
+    {
+        $this->handle = fopen($filename, 'a');
+    }
+
+    public function logEntry(string $line): void
+    {
+        fwrite($this->handle, $line . "\n");
+    }
+
+    public function __destruct()
+    {
+        fclose($this->handle);
+    }
+}
